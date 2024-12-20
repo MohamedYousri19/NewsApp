de74ef737dd8f739b4b218b728e26529c593bd9a
Mohamed Yousri <elmohamdy321@gmail.com>
2024-12-20 19:45:17 +0200

    first commit

---
+++ lib/moduoles/NewsApp/science_screen/Science_screen.dart
@@ -0,0 +1,30 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../Shared/Components/Components.dart';
+import '../../../layout/NewsApp/Cubit/cubit.dart';
+import '../../../layout/NewsApp/Cubit/states.dart';
+
+class Science_Screen extends StatelessWidget {
+  const Science_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<NewsCubit,NewsStates>(
+      listener: (context,state){},
+      builder: (context,state){
+        var list = NewsCubit.get(context).Science;
+        return ConditionalBuilder(
+          condition: list.isNotEmpty ,
+          builder: (context)=> ListView.separated(
+              itemBuilder: (context, index) => build_list_news(list[index],context),
+              separatorBuilder: (context, index) => line(),
+              itemCount: list.length
+          ),
+          fallback: (context)=>  Center(child: CircularProgressIndicator(color: Colors.orange,)),
+        );
+      },
+    ) ;
+  }
+}
